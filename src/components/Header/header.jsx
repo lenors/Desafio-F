9de60cfcac02dd591831619f1d179a9dc53b72fb
Leno rafael <73203800+lenors@users.noreply.github.com>
2024-12-02 17:39:39 +0000

    Repositorio Desafio Final
    Desafio FInal Vnw

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,42 @@
+import logo from "../../assets/logo.png";
+import busca from "../../assets/busca.png";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Inicio from "../../Pages/Inicio/Inicio";
+import Doados from "../../Pages/Doados/Doados";
+import QueroDoar from "../../Pages/QueroDoar/QueroDoar";
+import S from "./header.module.scss";
+
+export default function header() {
+  return (
+    <BrowserRouter>
+      <header>
+        <section className={S.caixaLogo}>
+          <img src={logo} alt="imagem " />
+          <h1>Livros Vai na Web</h1>
+        </section>
+        <nav className={S.boxMenu}>
+          <ul>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/doados">Livros Doados</Link>
+            </li>
+            <li>
+              <Link to="/queroDoar">Quero Doar</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={S.boxInput}>
+          <input type="text" placeholder="O que você procura?" />
+          <img src={busca} alt="" />
+        </div>
+      </header>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/doados" element={<Doados />} />
+        <Route path="/queroDoar" element={<QueroDoar />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
